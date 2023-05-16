54bc17dde042ff17b439306d27dc8434cae120b2
alessandrobozo <alessandrobozzi@yahoo.com>
2023-05-16 23:57:42 +0200

    Initial Commit

---
+++ Test App/Test_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Test_AppApp.swift
+//  Test App
+//
+//  Created by Alessandro Bozzi on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct Test_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
